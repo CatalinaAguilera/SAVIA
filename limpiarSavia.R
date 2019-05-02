a6fb839f1b1864c40f7046f78dd206046407b858
CatalinaAguilera <49456246+CatalinaAguilera@users.noreply.github.com>
2019-05-02 10:54:34 -0400

    Add files via upload

---
+++ limpiarSavia.R
@@ -0,0 +1,60 @@
+library(openxlsx)
+
+# Lectura archivo
+Datos = read.xlsx("D://Documentos//Trabajo Utal//SAVIA//Sap flow viña 2018-2019.xlsx",sheet = 1)
+
+# Duplicar para no utilizar la informacion original
+x <- Datos
+
+# Reemplazo datos
+# Noche = 480
+# eliminar 480 de dia y datos muy altos
+for(i in 4:27){
+  x[,i] <- replace(x[,i],x$`hh:mm`>= 0 & x$`hh:mm`<= 800,480)
+  x[,i] <- replace(x[,i],x$`hh:mm`>= 2100 & x$`hh:mm`<= 2330,480)
+  x[,i] <- replace(x[,i],x$`hh:mm`>800 & x$`hh:mm`<2100 & x[,i]==480,NA)
+  x[,i] <- replace(x[,i],x$`hh:mm`>=1030 & x$`hh:mm`<=1930 & x[,i]>=150,NA)
+}
+write.xlsx(x,"D://Documentos//Trabajo Utal//SAVIA//prueba.xlsx",header =T)
+remove(i)
+
+for(i in 4:27){
+  if(is.na(x[1,i])){
+    x[1,i] <- rowMeans(x[1,4:(i-1)])
+  }
+}
+remove(i)
+write.xlsx(x,"D://Documentos//Trabajo Utal//SAVIA//prueba1.xlsx",header =T)
+
+col2 <- vector()
+interpol <- Datos[,1:3]
+
+for(i in 4:27){
+  col2[1] <- x[1,i]
+  interpol <- cbind(interpol,INTERPOLATION(x[,i]))
+  colnames(interpol)[i] = colnames(x)[i]
+}
+write.xlsx(interpol,"D://Documentos//Trabajo Utal//SAVIA//prueba2.xlsx",header =T)
+
+### FUNCION INTERPOLACION ###
+
+INTERPOLATION <- function(col){
+  count <- 0
+  for(i in 2:length(col)){
+    if(is.na(col[i])){
+      count <- count + 1
+    }else{
+      if(count == 0){
+        col2[i] <- col[i]
+      }else{
+        for(j in 0:(count-1)){
+          col2[i-count+j] <- col2[i-count+j-1] + (col[i]-col2[i-count+j-1])/(count-j+1)
+          col2[i] <- col[i]
+        }
+        count <- 0
+      }
+    }
+  }
+  assign("col2", col2, envir = .GlobalEnv)
+}
+
